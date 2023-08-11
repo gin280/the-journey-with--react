178a7bd649cf2348dcd8ccf44cbd2c0f46724d1f
gin280 <zxy_wtfm@163.com>
2023-08-11 17:19:21 +0800

    feature: 37-38-user-list

---
+++ 37-38-user-list/work/src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => setUsers(json))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <>
+      <h1>User List</h1>
+      <ul>
+        {loading
+          ? "Loading"
+          : users.map((user) => <Item key={user.id} name={user.name} />)}
+      </ul>
+    </>
+  );
+}
+
+function Item({ name }) {
+  return <li>{name}</li>;
+}
+
+export default App;
